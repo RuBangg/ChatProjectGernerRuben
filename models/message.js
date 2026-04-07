811e62d659092bab5adcc4187992831274bbd33b
RuBangg <nielsruben91@gmail.com>
2026-04-07 09:47:34 +0200

    MessageClass

---
+++ models/message.js
@@ -0,0 +1,10 @@
+class Message{
+    constructor(besked,User,Chat){
+        this.besked = besked
+        this.User = User
+        this.id = Message.id++
+        this.Chat = Chat
+
+    }
+    static id = 1
+}
